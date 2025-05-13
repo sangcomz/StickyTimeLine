35f03fc4a59188bf0ea806e992ba63683475faa0
sangcomz <dev.seokwon2@gmail.com>
2025-05-13 23:54:12 +0900

    feat: Implement Vertical StickyTimeLine for Compose

--- sample-compose/src/main/java/io/github/sangcomz/stickytimeline/compose/MainActivity.kt
+++ sample-compose/src/main/java/io/github/sangcomz/stickytimeline/compose/MainActivity.kt
@@ -3,13 +3,31 @@ package io.github.sangcomz.stickytimeline.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.sangcomz.stickytimeline.compose.ui.theme.StickyTimeLineTheme
+import io.github.sangcomz.stickytimeline.data.Singer
+import io.github.sangcomz.stickytimeline.data.SingerRepo
 import io.github.sangcomz.stickytimelineview.compose.StickyTimeLineView
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +35,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StickyTimeLineTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val singers = SingerRepo().singerList
+                    StickyTimeLineView(
+                        groupedItems = singers.groupBy { it.debuted }.toSortedMap(),
+                        sectionHeader = { year, firstSinger ->
+                            SectionHeader(year = year, group = firstSinger.group)
+                        },
+                        itemContent = { singer ->
+                            SingerItem(singer = singer)
+                        }
+                    )
                 }
             }
         }
@@ -30,14 +56,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    StickyTimeLineView()
+fun SectionHeader(
+    year: String,
+    group: String,
+) {
+    Surface(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.wrapContentHeight().padding(8.dp)
+        ) {
+            Column() {
+                Text(
+                    text = year,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF414FCA)
+                    )
+                )
+                Text(
+                    text = group,
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = Color(0xFFD16767)
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SingerItem(singer: Singer) {
+    Row(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(top = 4.dp, bottom = 4.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        Card(
+            modifier = Modifier.wrapContentSize(),
+            shape = RoundedCornerShape(8.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            )
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(
+                    text = singer.name,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StickyTimeLineTheme {
-        Greeting("Android")
+
     }
 }
--- stickytimelineview-compose/src/main/java/io/github/sangcomz/stickytimelineview/compose/StickyTimeLineView.kt
+++ stickytimelineview-compose/src/main/java/io/github/sangcomz/stickytimelineview/compose/StickyTimeLineView.kt
@@ -1,26 +1,98 @@
 package io.github.sangcomz.stickytimelineview.compose
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun StickyTimeLineView() {
-    Text(
-        text = "Hello",
-        modifier = Modifier
-    )
-    LazyColumn {
-        items(100) {
-            Box {
-                Text("Item $it")
+fun <T> StickyTimeLineView(
+    groupedItems: Map<String, List<T>>,
+    modifier: Modifier = Modifier,
+    timeLineDot: @Composable () -> Unit = { TimelineDot() },
+    sectionHeader: @Composable (key: String, firstItem: T) -> Unit,
+    itemContent: @Composable (item: T) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(24.dp)
+                .align(Alignment.TopStart)
+        ) {
+            val centerX = size.width / 2
+            drawLine(
+                color = Color(0xFFB71C1C),
+                start = Offset(centerX, 0f),
+                end = Offset(centerX, size.height),
+                strokeWidth = 4.dp.toPx()
+            )
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            groupedItems.forEach { (key, items) ->
+                stickyHeader {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                    ) {
+                        Box {
+                            Spacer(Modifier.size(24.dp))
+                            timeLineDot()
+                        }
+
+                        sectionHeader(key, items.first())
+                    }
+                }
+                itemsIndexed(items) { _, item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        verticalAlignment = Alignment.Top
+                    ) {
+                        Spacer(Modifier.size(24.dp))
+                        itemContent(item)
+                    }
+                }
             }
         }
     }
-    Button(onClick = { /*TODO*/ }) {
+}
 
+@Composable
+fun TimelineDot(dotColor: Color = Color(0xFFB71C1C)) {
+    Box(
+        modifier = Modifier
+            .width(24.dp)
+            .fillMaxHeight(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(12.dp)
+                .background(dotColor, shape = androidx.compose.foundation.shape.CircleShape)
+        )
     }
 }
